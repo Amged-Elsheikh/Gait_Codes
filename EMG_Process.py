ea096f48b9fb73c5fa1e2b27d09c6363718a5fb9
Amged <amgedelshiekh@gmil.com>
2021-10-14 15:16:45 +0900

    hard code inputs names

--- EMG_Process.py
+++ EMG_Process.py
@@ -28,23 +28,28 @@ plt.rcParams["figure.figsize"] = [14, 10]
 
 # if subject==None:
 # subject = input("Please input subject number in XX format: ")
-subject='02'
+subject = '02'
 # Load settings file
-settings = pd.read_csv(f"../settings/emg_settings/S{subject}_emg_settings.csv", header=None)
-inputs_path = settings.iloc[0,1]
-outputs_path = settings.iloc[1,1]
+settings = pd.read_csv(
+    f"../settings/emg_settings/S{subject}_emg_settings.csv", header=None)
+inputs_path = settings.iloc[0, 1]
+outputs_path = settings.iloc[1, 1]
 # ## Functions
 
 # In[26]:
 
 
 def get_emg_files():
-    inputs_names = os.listdir(inputs_path)
+    # Get normalize file
     norm_file = f"S{subject}_EMG_norm.csv"
-    inputs_names.remove(norm_file)
-    output_files = list(map(lambda x:f"{outputs_path}{x}", inputs_names))
-    output_files = list(map(lambda x:x.replace("_EMG","_features"), output_files))
+    # Get inputs names
+    inputs_names = ["test", "train_01", "train_02", "val"]
+    inputs_names = list(map(lambda x: f"S{subject}_{x}_EMG.csv", inputs_names)) # add path, subject number and file extension
+    # Get outputs names
+    output_files = list(map(lambda x: f"{outputs_path}{x}", inputs_names))
+    output_files = list(map(lambda x: x.replace("_EMG", "_features"), output_files))
     return norm_file, inputs_names, output_files
+
 
 def load_emg_data(emg_file):
     emg = pd.read_csv(f"{inputs_path}{emg_file}", header=12)
@@ -52,16 +57,18 @@ def load_emg_data(emg_file):
     emg.columns = emg.columns.str.replace("Trigno IM ", "", regex=True)
     emg.columns = emg.columns.str.replace("X [s]", "time", regex=False)
     emg.set_index("time", inplace=True)
-    ## work with EMG signals from the Shank only
-    emg = emg[[f"sensor {i}" for i in range(7,13)]]
+    # work with EMG signals from the Shank only
+    emg = emg[[f"sensor {i}" for i in range(7, 13)]]
     return emg
 
+
 def apply_filter(emg, order=4, lowband=20, highband=450):
-    fs = 1/0.0009 # Hz
+    fs = 1/0.0009  # Hz
     low_pass = lowband/(fs*0.5)
     hig_pass = highband/(fs*0.5)
     b, a = butter(N=order, Wn=[low_pass, hig_pass], btype="bandpass")
     return filtfilt(b, a, emg)
+
 
 def normalize_emg(filtered_emg, norm_file):
     """
@@ -74,17 +81,20 @@ def normalize_emg(filtered_emg, norm_file):
     # Remove the mean
     filtered_emg = filtered_emg - filtered_emg.mean()
     # Create a pandas dataset that will received the normalized data
-    normalized_emg = pd.DataFrame(index=filtered_emg.index, columns=filtered_emg.columns)
+    normalized_emg = pd.DataFrame(
+        index=filtered_emg.index, columns=filtered_emg.columns)
     for col in filtered_emg.columns:
         # Create standard scaler object
-        scaler=MinMaxScaler(feature_range=(-1,1))
+        scaler = MinMaxScaler(feature_range=(-1, 1))
         # fit the scaler to the normailer data
-        scaler.fit(np.reshape(emg_normalizer[col].values,(-1,1)))
+        scaler.fit(np.reshape(emg_normalizer[col].values, (-1, 1)))
         # Get one sensor values at the time
         filtered_emg_col = filtered_emg[col].values
         # Normalize the column and updated normalized emg dataframe values
-        normalized_emg[col] = scaler.transform(np.reshape(filtered_emg_col, (-1,1)))
+        normalized_emg[col] = scaler.transform(
+            np.reshape(filtered_emg_col, (-1, 1)))
     return normalized_emg
+
 
 def process_emg_signal(emg, norm_file):
     # filter the signals
@@ -97,12 +107,13 @@ def process_emg_signal(emg, norm_file):
     DEMG = norm_emg.apply(np.gradient)/0.0009
     return DEMG
 
+
 def plot_all_emg(emg, file_name=None):
-    m = 2 # number of columns
-    n = int(len(emg.columns)/2) # number of raws
+    m = 2  # number of columns
+    n = int(len(emg.columns)/2)  # number of raws
     for i in range(n):
-        plt.subplot(n,m,i+1)
-        plt.plot(emg.index, emg.iloc[:,2*i], emg.index, emg.iloc[:,2*i+1])
+        plt.subplot(n, m, i+1)
+        plt.plot(emg.index, emg.iloc[:, 2*i], emg.index, emg.iloc[:, 2*i+1])
     plt.suptitle(file_name)
     plt.show()
 
@@ -110,31 +121,26 @@ def plot_all_emg(emg, file_name=None):
 # ### Features Functions
 
 # In[27]:
-    
-def get_AR_coeffs(data, num_coeff = 6):
-    model = AutoReg (data, lags=num_coeff, )#old_names=True)
+
+def get_AR_coeffs(data, num_coeff=6):
+    model = AutoReg(data, lags=num_coeff, )  # old_names=True)
     model_fit = model.fit()
     return model_fit.params
 
-# def get_RMS(data):
-#     return sqrt(sum(n*n for n in data)/len(data))
 
 def get_MAV(data):
     return sum(abs(n) for n in data)/len(data)
 
+
 def zero_crossing(data):
-    zero_crossings = np.where(np.diff(np.signbit(data)))[0] # returns the indexes of where ZC appear
+    # returns the indexes of where ZC appear
+    zero_crossings = np.where(np.diff(np.signbit(data)))[0]
     return len(zero_crossings)
+
 
 def wave_length(data):
     return np.sum(abs(np.diff(data)))
 
-
-# In[47]:
-def create_empty_array(DEMG, col_num=1, factor=10):
-    array = np.empty((int(len(DEMG)/factor), col_num))
-    array[:] = np.nan
-    return array
 
 def get_features(DEMG):
     step = 0.05
@@ -150,9 +156,10 @@ def get_features(DEMG):
         EMG_label = f"sensor {EMG_num+7}"
         sensor_data = DEMG[EMG_label]
 
-        while (end<time_limit):
-            window_data = sensor_data[(DEMG.index>=start) & (DEMG.index < end)].to_numpy()
-            #Get the AR coefficients
+        while (end < time_limit):
+            window_data = sensor_data[(DEMG.index >= start) & (
+                DEMG.index < end)].to_numpy()
+            # Get the AR coefficients
             coeff.append(get_AR_coeffs(window_data))
             # Get the MAV
             MAV.append(get_MAV(window_data))
@@ -165,23 +172,24 @@ def get_features(DEMG):
         coeff = np.array(coeff)
         ZC = np.array(ZC)
         MAV = np.array(MAV)
-        dataset_temp =  pd.DataFrame({f'DEMG{EMG_num+1}_AR1':coeff[:,1], f'DEMG{EMG_num+1}_AR2':coeff[:,2], 
-                                      f'DEMG{EMG_num+1}_AR3':coeff[:,3],f'DEMG{EMG_num+1}_AR4':coeff[:,4], 
-                                      f'DEMG{EMG_num+1}_AR5':coeff[:,5], f'DEMG{EMG_num+1}_AR6':coeff[:,6],
-                                      f'DEMG{EMG_num+1}_ZC': ZC, f'DEMG{EMG_num+1}_MAV': MAV})
-        dataset = pd.concat([dataset, dataset_temp], axis = 1)
+        dataset_temp = pd.DataFrame({f'DEMG{EMG_num+1}_AR1': coeff[:, 1], f'DEMG{EMG_num+1}_AR2': coeff[:, 2],
+                                     f'DEMG{EMG_num+1}_AR3': coeff[:, 3], f'DEMG{EMG_num+1}_AR4': coeff[:, 4],
+                                     f'DEMG{EMG_num+1}_AR5': coeff[:, 5], f'DEMG{EMG_num+1}_AR6': coeff[:, 6],
+                                     f'DEMG{EMG_num+1}_ZC': ZC, f'DEMG{EMG_num+1}_MAV': MAV})
+        dataset = pd.concat([dataset, dataset_temp], axis=1)
 #         print(f"{EMG_label} done")
 
     dataset['time'] = [np.around(step*i, 2) for i in range(len(dataset))]
     dataset.set_index("time", inplace=True)
     dataset.describe()
     return dataset
-    
+
+
 def plot_MAV(dataset, emg_file):
     MAV_columns = [f'DEMG{i+1}_MAV' for i in range(6)]
     MAV_data = dataset[MAV_columns]
     plot_all_emg(MAV_data, emg_file)
-    
+
 # In[48]:
 
 
@@ -193,8 +201,9 @@ def emg_to_features():
     #     plot_all_emg(DEMG, emg_file)
         dataset = get_features(DEMG)
         plot_MAV(dataset, emg_file)
-        plt.show();
+        plt.show()
         dataset.to_csv(output_file)
+
 
 # %%time
 emg_to_features()
