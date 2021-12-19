af88c2f545659616800e011a3608990d135401ba
Amged <amgedelshiekh@gmil.com>
2021-12-19 13:05:06 +0900

    Start adding docstring

--- MotionConvert .py
+++ MotionConvert .py
@@ -3,28 +3,41 @@ Created on Thu Jul 29 14:26:07 2021
 
 @author: amged
 """
-
+# Import libraries
 import pandas as pd
 import json
 import os
-## Load subject details
-with open("subject_details.json","r") as f:
-    subject_details = json.load(f)
+
 
 def get_IO_dir(subject=None, motion_type="dynamic"):
-    if subject==None:
+    """[summary]
+
+    Args:
+        subject (string, optional): [Subject number in the format XX. If the value is\
+            None then user will be asked manually input subject's number]. Defaults to None.
+        motion_type (str, optional): [Either 'static' or 'dynamic']. Defaults to "dynamic".
+
+    Returns:
+        [list]: [a list contains inputs files directories.]
+        [list]: [a list contains outputs files directories]
+    """
+    if subject == None:
         subject = input("insert subject number: ")
     # Create motion Setting File
     date = subject_details[f"S{subject}"]["date"]
     if motion_type == "static":
-        Inputs =  f"../Data/S{subject}/{date}/Statics/S{subject}_static.csv" # Inputs folder
-        Outputs = f"../OpenSim/S{subject}/{date}/Statics/S{subject}_static.trc"  # Outputs folder
-        return Inputs, Outputs
+        # Inputs file path
+        Inputs = [f"../Data/S{subject}/{date}/Statics/S{subject}_static.csv"]
+        # Outputs file path
+        Outputs = [
+            f"../OpenSim/S{subject}/{date}/Statics/S{subject}_static.trc"]
 
     elif motion_type == "dynamic":
-        input_path = f"../Data/S{subject}/{date}/Dynamics/motion_data/"  # Inputs folder
-        output_path = f"../OpenSim/S{subject}/{date}/Dynamics/motion_data/"  # Outputs folder
-        # Get files names
+        # Inputs folder path
+        input_path = f"../Data/S{subject}/{date}/Dynamics/motion_data/"
+        # Outputs folder path
+        output_path = f"../OpenSim/S{subject}/{date}/Dynamics/motion_data/"
+        # Get files names (trials)
         trials = ["train_01", "train_02", "val", "test"]
         Inputs = list(map(lambda x: f"S{subject}_{x}.csv", trials))
         Outputs = list(map(lambda x: f"{x}".replace('csv', 'trc'), Inputs))
@@ -32,7 +45,7 @@ def get_IO_dir(subject=None, motion_type="dynamic"):
         # Get inputs and outputs full directories
         Inputs = list(map(lambda file: input_path+file, Inputs))
         Outputs = list(map(lambda file: output_path+file, Outputs))
-        return Inputs, Outputs
+    return Inputs, Outputs
 
 
 def get_markers_labels(Input, Markers_Set_Name="Amged:"):
@@ -48,7 +61,7 @@ def get_markers_labels(Input, Markers_Set_Name="Amged:"):
         # Need to remove the Marker set name
         Temp_1 = Temp.replace(Markers_Set_Name, "")
         Markers_Label.append(Temp_1)
-#     print("Number of labels: ",len(Markers_Label))
+    # print("Number of labels: ",len(Markers_Label))
     return Markers_Label
 
 
@@ -65,7 +78,7 @@ def convert_data(Input, Output):
     return num_frames
 
 
-def process_trc(Output, Markers_Label, num_frames, Markers_number=39):
+def process_trc(Output, Markers_Label, num_frames):
     New_label_Coor = '\t'
     New_label_Mar = 'Frame#\tTime'
 
@@ -94,16 +107,21 @@ def csv2trc(subject=None):
         print(f"{motion_type} Data")
         Inputs, Outputs = get_IO_dir(subject, motion_type=motion_type)
 
-        if motion_type=="dynamic":
-            for Input, Output in zip(Inputs, Outputs):
-                Markers_Label = get_markers_labels(Input)
-                num_frames = convert_data(Input, Output)
-                process_trc(Output, Markers_Label, num_frames)
-        else:
-            Markers_Label = get_markers_labels(Inputs)
-            num_frames = convert_data(Inputs, Outputs)
-            process_trc(Outputs, Markers_Label, num_frames)
+        # if motion_type == "dynamic":
+        for Input, Output in zip(Inputs, Outputs):
+            Markers_Label = get_markers_labels(Input)
+            num_frames = convert_data(Input, Output)
+            process_trc(Output, Markers_Label, num_frames)
+        # else:
+        #     Markers_Label = get_markers_labels(Inputs)
+        #     num_frames = convert_data(Inputs, Outputs)
+        #     process_trc(Outputs, Markers_Label, num_frames)
 
 
 Markers_number = 39
-csv2trc()
+
+if __name__ == '__main__':
+    # Load subject details
+    with open("subject_details.json", "r") as f:
+        subject_details = json.load(f)
+    csv2trc()
