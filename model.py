406afd716ae92b842409c617cb72056b3e70ef90
Amged <amgedelshiekh@gmil.com>
2021-12-19 13:07:49 +0900

    Move all tf models/functions to a new file

--- model.py
+++ model.py
@@ -1,9 +1,8 @@
-from Custom.WindowGenerator import WindowGenerator
 # import matplotlib as mpl
+from Custom.WindowGenerator import WindowGenerator
 from functools import partial
 from sklearn import metrics
 import matplotlib.pyplot as plt
-from tensorflow.keras import layers
 import os
 import pandas as pd
 import numpy as np
@@ -11,6 +10,7 @@ import json
 from sklearn.preprocessing import MinMaxScaler
 import tensorflow as tf
 from tensorflow import keras
+from tensorflow_models import *
 K = keras.backend
 # import os
 # os.environ["MKL_THREADING_LAYER"] = "GNU"
@@ -18,99 +18,32 @@ K = keras.backend
 
 # mpl.rcParams['figure.dpi'] = 110
 
-model_dic = {}
 
-if not tf.test.is_built_with_cuda():
-    raise print("No GPU found")
+# Scaling function
 
-with open("subject_details.json", "r") as f:
-    subject_details = json.load(f)
+def scale_function(data, weight, features_scaler, angle_scaler):
+    """This function will scale the dataset, if you do not want to scale something, set it's value to None.
 
-
-# Scaling functions
-def scale_moment(data, weight, scale=True):
-    if scale:
+    Args:
+        data ([pandas dataframe]): [The data columns are arranged as fellow: EMG features, angles and moments]
+        weight ([float or None]): [subject weight. If None, no scale will be performed.]
+        features_scaler ([sci-kit learn scaler]): [Scale the features. Use MinMax scaler. If None, no scale will be performed.]
+        angle_scaler ([sci-kit learn scaler]): [Scale the angles. Use MinMax scaler. If None, no scale will be performed.]
+    """
+    # Scale moments
+    if weight:
         data.iloc[:, -4:] = data.iloc[:, -4:]/weight
-    return data
-
-
-def scale_angle(data, angle_scaler, scale=True):
-    if scale:
+    # Scale features
+    if features_scaler:
+        data.iloc[:, :-8] = features_scaler.transform(data.iloc[:, :-8])
+    # Scale angles
+    if angle_scaler:
         columns = data.columns[-8:-4]
         data[columns] = angle_scaler.transform(data[columns])
-    return data
+    return
 
 
-def scale_features(data, scaler):
-    data.iloc[:, :-8] = scaler.transform(data.iloc[:, :-8])
-    return data
-
-# window-object is a custom object
-
-
-def make_dataset(window_object):
-    train_set = window_object.get_train_dataset()
-    val_set = window_object.get_val_dataset()
-    test_set = window_object.get_evaluation_set()
-    return train_set, val_set, test_set
-
-
-def custom_loss(y_true, y_pred):
-    error = tf.reshape(y_true - y_pred, (-1, 1))
-    error = error[~tf.math.is_nan(error)]
-    return tf.reduce_mean(tf.square(error), axis=0)
-
-# Custom evaluation functions
-
-
-def nan_R2(y_true, y_pred):
-    R2 = []
-    _, l = np.shape(y_true)
-    for i in range(l):
-        y_true_col = y_true[:, i]
-        y_pred_col = y_pred[:, i]
-        logic = np.isfinite(y_true_col)
-        y_true_col = y_true_col[logic]
-        y_pred_col = y_pred_col[logic]
-        R2.append(metrics.r2_score(y_true_col, y_pred_col))
-    return np.around(R2, 4)
-
-
-def nan_rmse(y_true, y_pred):
-    MSE = partial(metrics.mean_squared_error, squared=False)
-    rmse = []
-    _, l = np.shape(y_true)
-    for i in range(l):
-        y_true_col = y_true[:, i]
-        y_pred_col = y_pred[:, i]
-        logic = np.isfinite(y_true_col)
-        y_true_col = y_true_col[logic]
-        y_pred_col = y_pred_col[logic]
-        rmse.append(MSE(y_true_col, y_pred_col))
-    return np.around(rmse, 2)
-
-# Plotting function
-
-
-def plot_learning_curve(history):
-    if history == None:
-        print("No train history was found")
-        return None
-    else:
-        plt.figure("Learning curve")
-        plt.plot(history.epoch, history.history['loss'],
-                 history.epoch, history.history['val_loss'])
-        plt.legend(["train loss", "val loss"])
-        plt.xlabel("Epochs")
-        plt.ylabel("loss")
-        plt.draw()
-        plt.savefig(f"{folder}learning_curve.pdf")
-        # plt.close()
-        return None
-
-
-def plot_results(window_object, y_true, y_pred, R2_score, rmse_result):
-    global time
+def plot_results(window_object, y_true, y_pred, R2_score, rmse_result, folder):
     time = [i/20 for i in range(len(y_true))]
     plt.figure("Prediction")
     labels = window_object.out_labels
@@ -121,16 +54,19 @@ def plot_results(window_object, y_true, y_pred, R2_score, rmse_result):
         plt.plot(time, y_true[:, i], linewidth=2.5)
         plt.plot(time, y_pred[:, i], "r--", linewidth=2.5,)
         plt.title(col)
-        plt.legend(["y_true", "y_pred"])
+        if i == 0:
+            plt.legend(["y_true", "y_pred"], loc='lower left')
         plt.xlim((time[-600], time[-100]))
         plt.xlabel("Time [s]")
         if "moment" in col:
             plt.ylabel("Moment [Nm]")
         else:
             plt.ylabel("Angle [Degree]")
+    plt.tight_layout()
+    plt.savefig(f"{folder}{labels[i]}.svg")
     plt.savefig(f"{folder}{labels[i]}.pdf")
     plt.draw()
-    # plt.close()
+    plt.close()
 
 
 def choose_features(data, features=["RMS"]):
@@ -141,7 +77,7 @@ def choose_features(data, features=["RMS"]):
                 if feature.lower() in col.lower():
                     new_columns.append(col)
                     continue
-        else: # append all labels
+        else:  # append all labels
             new_columns.append(col)
     return data[new_columns]
 
@@ -151,39 +87,44 @@ def create_window_generator(subject=None):
         subject = input("Please input subject number in XX format: ")
     if len(subject) == 1:
         subject = "0" + subject
+    # #Get subject weight.
     w = subject_details[f'S{subject}']['weight']
-    dataset_folder = f"../Dataset/S{subject}/"
+    # #Get trials directory
     trials = ["train_01", "train_02", "val", "test"]
-    trials = list(map(lambda x: f"{dataset_folder}{x}_dataset.csv", trials))
-    train_01_df = scale_moment(pd.read_csv(
-        trials[0], index_col='time'), weight=w)
-    train_02_df = scale_moment(pd.read_csv(
-        trials[1], index_col='time'), weight=w)
-    val_df = scale_moment(pd.read_csv(trials[2], index_col='time'), weight=w)
-    test_df = scale_moment(pd.read_csv(trials[3], index_col='time'), weight=w)
-
-    scaler = MinMaxScaler(feature_range=(-1, 1))
-    scaler.fit(train_01_df.iloc[:400, :-8])
+    trials = list(
+        map(lambda x: f"../Dataset/S{subject}/{x}_dataset.csv", trials))
+    # #Load data
+    train_01_df = pd.read_csv(trials[0], index_col='time'),
+    train_02_df = pd.read_csv(trials[1], index_col='time')
+    val_df = pd.read_csv(trials[2], index_col='time')
+    test_df = pd.read_csv(trials[3], index_col='time')
+    # #Prepare scalers
+    features_scaler = MinMaxScaler(feature_range=(0, 1))
+    features_scaler.fit(train_01_df.iloc[:400, :-8])
     angle_scaler = MinMaxScaler(feature_range=(0, 1))
     angle_scaler.fit(train_01_df.iloc[:400, -8:-4])
+    # #Scale the dataset
     for data in [train_01_df, train_02_df, val_df, test_df]:
-        data = scale_features(data, scaler)
-        data = scale_angle(data, angle_scaler)
-    
+        data = scale_function(
+            data, weights=w, features_scaler=features_scaler, angle_scaler=angle_scaler)
+
     train_01_df = choose_features(train_01_df, features=features)
     train_02_df = choose_features(train_02_df, features=features)
     val_df = choose_features(val_df, features=features)
     test_df = choose_features(test_df, features=features)
-    # Create Window object
-    window_object = WindowGenerator(train_01_df=train_01_df, train_02_df=train_02_df,
-                                    val_df=val_df, test_df=test_df, batch_size=64,
-                                    input_width=15, shift=1, label_width=1)
+    # #Create Window object
+    window_object = WindowGenerator(train_01_df, train_02_df, val_df, test_df,
+                                    input_width=15, shift=3, label_width=1,
+                                    batch_size=64, add_knee=add_knee, out_labels=out_labels)
     return window_object
 
 
-def train_fit(subject, model_name, epochs=1, lr=0.001, eval_only=False, load_best=False):
+def train_fit(subject, tested_on, model_name, epochs=1, lr=0.001, eval_only=False, load_best=False):
 
     window_object = create_window_generator(subject)
+    if tested_on == None:
+        tested_on = subject
+
     # setup model folder
     global folder
     folder = f"../Results/indiviuals/{model_name}/S{subject}/"
@@ -192,7 +133,7 @@ def train_fit(subject, model_name, epochs=1, lr=0.001, eval_only=False, load_bes
 
     w = subject_details[f'S{subject}']['weight']
     # Get all dataset
-    train_set, val_set, test_set = make_dataset(window_object)
+    train_set, val_set, _ = window_object.make_dataset()
     ##############################################################################################################
     # Load and compile new model
     K.clear_session()
@@ -215,7 +156,7 @@ def train_fit(subject, model_name, epochs=1, lr=0.001, eval_only=False, load_bes
         if not eval_only:
             history = model.fit(x=train_set, validation_data=val_set,
                                 epochs=epochs, callbacks=[model_checkpoint_callback])
-            plot_learning_curve(history)
+            plot_learning_curve(history, folder)
             # Load the best model
             model.load_weights(f"{folder}/S{subject}_{model_name}.hdf5")
         else:
@@ -228,83 +169,52 @@ def train_fit(subject, model_name, epochs=1, lr=0.001, eval_only=False, load_bes
         print("No saved model existing. weights will be initialized")
     ##############################################################################################################
     # Get predictions and real values
+    window_object = create_window_generator(tested_on)
+    _, _, test_set = window_object.make_dataset()
     y_pred = model.predict(test_set)
     if len(y_pred.shape) == 3:
+        # Get the last time step and reduce output dimenions to two
         y_pred = y_pred[:, -1, :]
     # Get real outputs
     for _, y_true in test_set.as_numpy_iterator():
         break
     y_true = y_true[:, -1, :]
-    ##############################################################################################################
-    ## Evaluation and plot
-    r2_score = nan_R2(w*y_true, w*y_pred)
-    rmse_result = nan_rmse(w*y_true, w*y_pred)
-    plot_results(window_object, y_true, y_pred, r2_score, rmse_result)
-    plt.show()
+
+    ################ Evaluation and plot ################
+    r2_score = nan_R2(y_true, y_pred)
+    rmse_result = nan_rmse(y_true, y_pred)
+    folder = f"../Results/indiviuals/{model_name}/S{tested_on}/"
+    plot_results(window_object, y_true, y_pred, r2_score, rmse_result, folder)
+    plt.draw()
     return history, y_true, y_pred, r2_score, rmse_result
 
-# #Models
-
-def create_lstm_model(window_object):
-    # kernel_regularizer='l2', recurrent_regularizer='l2', activity_regularizer='l2')
-    # kernel_regularizer='l2', recurrent_regularizer='l2', activity_regularizer='l2')
-    custom_LSTM = partial(layers.LSTM, dropout=0.3,)
-    lstm_model = keras.models.Sequential([
-        layers.InputLayer((window_object.input_width,
-                          window_object.features_num)),
-        # layers.BatchNormalization(),
-        # custom_LSTM(4, return_sequences=True),
-        custom_LSTM(4, return_sequences=True),
-        custom_LSTM(4, return_sequences=False),
-        layers.Dense(window_object.out_nums * window_object.label_width),
-        layers.Reshape([window_object.label_width, window_object.out_nums])
-    ])
-    return lstm_model
-
-
-def create_conv_model(window_object):
-    conv_model = keras.models.Sequential([
-        layers.InputLayer((window_object.input_width,
-                          window_object.features_num)),
-        # layers.BatchNormalization(),
-        layers.Conv1D(filters=20, kernel_size=3, strides=1, padding='same'),
-        layers.BatchNormalization(),
-        layers.MaxPool1D(pool_size=2, strides=1, padding="valid"),
-        layers.Conv1D(filters=30, kernel_size=3, strides=1, padding='same'),
-        layers.BatchNormalization(),
-        layers.MaxPool1D(pool_size=2, strides=1, padding="valid"),
-        layers.Conv1D(filters=window_object.out_nums, kernel_size=1, strides=1)
-    ])
-    return conv_model
-
-
-def create_nn_model(window_object):
-    nn_model = keras.models.Sequential([
-        layers.InputLayer((window_object.input_width,
-                          window_object.features_num)),
-        layers.Flatten(),
-        layers.Dense(32),
-        layers.Dense(32),
-        layers.Dense(32),
-        layers.Dense(window_object.out_nums * window_object.label_width),
-        layers.Reshape([window_object.label_width, window_object.out_nums])
-    ])
-    return nn_model
-
-
-model_dic["lstm_model"] = create_lstm_model
-model_dic["conv_model"] = create_conv_model
-model_dic["nn_model"] = create_nn_model
 
 if __name__ == '__main__':
-    features = ["RMS","ZC"]
-    w1 = create_window_generator(subject="1")
-    w2 = create_window_generator(subject="2")
-    w4 = create_window_generator(subject="4")
+    if not tf.test.is_built_with_cuda():
+        raise print("No GPU found")
 
+    with open("subject_details.json", "r") as f:
+        subject_details = json.load(f)
+
+    features = ["RMS", "ZC"]
+    add_knee = False
+    out_labels = ["ankle moment"]
     model_name = "nn_model"
 
-# Train and test new/existing models
-for model_name in model_dic.keys():
-    history, y_true, y_pred, r2, rmse = train_fit("02", model_name, epochs=200, eval_only=True, load_best=True)
-    print(model_name)
+    model_dic = {}
+
+    model_dic["lstm_model"] = create_lstm_model
+    model_dic["conv_model"] = create_conv_model
+    model_dic["nn_model"] = create_nn_model
+
+    # w1 = create_window_generator(subject="1")
+    # w2 = create_window_generator(subject="2")
+    # w4 = create_window_generator(subject="4")
+
+    # Train and test new/existing models
+    for model_name in model_dic.keys():
+        history, y_true, y_pred, r2, rmse = train_fit(subject="02", tested_on="04",
+                                                      model_name=model_name, epochs=1000,
+                                                      eval_only=True, load_best=True)
+        print(model_name)
+        # plt.show()
