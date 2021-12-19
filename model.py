9c4d567bb67872385905916ab7c17457bebd3148
Amged Elsheikh <62318395+Amged-Elsheikh@users.noreply.github.com>
2021-12-19 20:02:57 +0900

    formatting

--- model.py
+++ model.py
@@ -10,7 +10,8 @@ import json
 from sklearn.preprocessing import MinMaxScaler
 import tensorflow as tf
 from tensorflow import keras
-from models_functions import *
+from Custom.models_functions import *
+
 K = keras.backend
 # import os
 # os.environ["MKL_THREADING_LAYER"] = "GNU"
@@ -18,22 +19,22 @@ K = keras.backend
 
 # mpl.rcParams['figure.dpi'] = 110
 
+
 def create_window_generator(subject=None):
     if subject == None:
         subject = input("Please input subject number in XX format: ")
     if len(subject) == 1:
         subject = "0" + subject
     # #Get subject weight.
-    w = subject_details[f'S{subject}']['weight']
+    w = subject_details[f"S{subject}"]["weight"]
     # #Get trials directory
     trials = ["train_01", "train_02", "val", "test"]
-    trials = list(
-        map(lambda x: f"../Dataset/S{subject}/{x}_dataset.csv", trials))
+    trials = list(map(lambda x: f"../Dataset/S{subject}/{x}_dataset.csv", trials))
     # #Load data
-    train_01_df = pd.read_csv(trials[0], index_col='time'),
-    train_02_df = pd.read_csv(trials[1], index_col='time')
-    val_df = pd.read_csv(trials[2], index_col='time')
-    test_df = pd.read_csv(trials[3], index_col='time')
+    train_01_df = (pd.read_csv(trials[0], index_col="time"),)
+    train_02_df = pd.read_csv(trials[1], index_col="time")
+    val_df = pd.read_csv(trials[2], index_col="time")
+    test_df = pd.read_csv(trials[3], index_col="time")
     # #Prepare scalers
     features_scaler = MinMaxScaler(feature_range=(0, 1))
     features_scaler.fit(train_01_df.iloc[:400, :-8])
@@ -42,44 +43,58 @@ def create_window_generator(subject=None):
     # #Scale the dataset
     for data in [train_01_df, train_02_df, val_df, test_df]:
         data = scale_function(
-            data, weights=w, features_scaler=features_scaler, angle_scaler=angle_scaler)
+            data, weights=w, features_scaler=features_scaler, angle_scaler=angle_scaler
+        )
 
     train_01_df = choose_features(train_01_df, features=features)
     train_02_df = choose_features(train_02_df, features=features)
     val_df = choose_features(val_df, features=features)
     test_df = choose_features(test_df, features=features)
     # #Create Window object
-    window_object = WindowGenerator(train_01_df, train_02_df, val_df, test_df,
-                                    input_width=15, shift=3, label_width=1,
-                                    batch_size=64, add_knee=add_knee, out_labels=out_labels)
+    window_object = WindowGenerator(
+        train_01_df,
+        train_02_df,
+        val_df,
+        test_df,
+        input_width=15,
+        shift=3,
+        label_width=1,
+        batch_size=64,
+        add_knee=add_knee,
+        out_labels=out_labels,
+    )
     return window_object
 
 
-def train_fit(subject, tested_on, model_name, epochs=1, lr=0.001, eval_only=False, load_best=False):
-
-    window_object = create_window_generator(subject)
-    if tested_on == None:
-        tested_on = subject
-
+def train_fit(
+    subject, tested_on, model_name, epochs=1, lr=0.001, eval_only=False, load_best=False
+):
     # setup model folder
     global folder
     folder = f"../Results/indiviuals/{model_name}/S{subject}/"
     if not os.path.exists(folder):
         os.makedirs(folder)
 
-    w = subject_details[f'S{subject}']['weight']
+    window_object = create_window_generator(subject)
+    if tested_on == None:
+        tested_on = subject
+
+    w = subject_details[f"S{subject}"]["weight"]
     # Get all dataset
     train_set, val_set, _ = window_object.make_dataset()
     ##############################################################################################################
     # Load and compile new model
     K.clear_session()
     model = model_dic[model_name](window_object)
-    model.compile(optimizer=keras.optimizers.Nadam(learning_rate=lr),
-                  loss="mean_squared_error")
+    model.compile(
+        optimizer=keras.optimizers.Nadam(learning_rate=lr), loss="mean_squared_error"
+    )
     model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
         filepath=f"{folder}S{subject}_{model_name}.hdf5",
-        save_weights_only=True, monitor='val_loss',
-        save_best_only=True)
+        save_weights_only=True,
+        monitor="val_loss",
+        save_best_only=True,
+    )
 
     if load_best:
         try:
@@ -90,8 +105,12 @@ def train_fit(subject, tested_on, model_name, epochs=1, lr=0.001, eval_only=Fals
     # Train Model
     try:  # Train or load the best model the model
         if not eval_only:
-            history = model.fit(x=train_set, validation_data=val_set,
-                                epochs=epochs, callbacks=[model_checkpoint_callback])
+            history = model.fit(
+                x=train_set,
+                validation_data=val_set,
+                epochs=epochs,
+                callbacks=[model_checkpoint_callback],
+            )
             plot_learning_curve(history, folder)
             # Load the best model
             model.load_weights(f"{folder}/S{subject}_{model_name}.hdf5")
@@ -120,12 +139,12 @@ def train_fit(subject, tested_on, model_name, epochs=1, lr=0.001, eval_only=Fals
     r2_score = nan_R2(y_true, y_pred)
     rmse_result = nan_rmse(y_true, y_pred)
     folder = f"../Results/indiviuals/{model_name}/S{tested_on}/"
-    plot_results(window_object, y_true, y_pred, r2_score, rmse_result, folder)
+    plot_results(y_true, y_pred, out_labels, r2_score, rmse_result, folder)
     plt.draw()
     return history, y_true, y_pred, r2_score, rmse_result
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if not tf.test.is_built_with_cuda():
         raise print("No GPU found")
 
@@ -149,8 +168,13 @@ if __name__ == '__main__':
 
     # Train and test new/existing models
     for model_name in model_dic.keys():
-        history, y_true, y_pred, r2, rmse = train_fit(subject="02", tested_on="04",
-                                                      model_name=model_name, epochs=1000,
-                                                      eval_only=True, load_best=True)
+        history, y_true, y_pred, r2, rmse = train_fit(
+            subject="02",
+            tested_on="04",
+            model_name=model_name,
+            epochs=1000,
+            eval_only=True,
+            load_best=True,
+        )
         print(model_name)
         # plt.show()
