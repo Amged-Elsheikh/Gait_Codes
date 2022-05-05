5c62df51519270af9619004e33abadbd47450339
Amged <amgedelshiekh@gmil.com>
2022-05-05 19:49:21 +0900

    fix plot generation

--- Custom/PlottingFunctions.py
+++ Custom/PlottingFunctions.py
@@ -19,7 +19,7 @@ def plot_learning_curve(history, folder):
         plt.savefig(f"{folder}learning_curve.pdf")
 
 
-def plot_results(y_true, y_pred, out_labels, R2_score, rmse_result, max_error, folder):
+def plot_results(y_true, y_pred, out_labels, R2_score, rmse_result, max_error, nrmse, folder):
     tick_size = 12
     label_size = 14
     title_size = 20
@@ -29,7 +29,9 @@ def plot_results(y_true, y_pred, out_labels, R2_score, rmse_result, max_error, f
         plt.subplot(len(out_labels), 1, i + 1)
         print(f"{col} R2 score: {R2_score[i]}")
         print(f"{col} RMSE result: {rmse_result[i]}")
-        print(f"{col} max error is {max_error}Nm/Kg")
+        print(f"{col} max error is {max_error[i]}Nm/Kg")
+        print(f"{col} nRMSE is {nrmse[i]}Nm/Kg")
+        
         plt.plot(time, y_true[:, i], linewidth=2.5)
         plt.plot(time, y_pred[:, i], "r--", linewidth=2,)
         plt.title(col, fontsize=title_size)
@@ -44,14 +46,13 @@ def plot_results(y_true, y_pred, out_labels, R2_score, rmse_result, max_error, f
         plt.xticks(fontsize=tick_size)
         plt.yticks(fontsize=tick_size)
         plt.grid(True)
-        plt.axhline(y = 0, color = 'black', linestyle = '-')
+        plt.axhline(y=0, color='black', linestyle='-')
     plt.tight_layout()
-    plt.savefig(f"{folder}{out_labels[i]}.svg")
-    plt.savefig(f"{folder}{out_labels[i]}.pdf")
+    plt.savefig(f"{folder}Predictions.pdf")
     plt.draw()
 
 
-def plot_models(predictions: dict, y_true, path: str, subject=None):
+def plot_models(predictions: dict, y_true, labels, subject, path: str):
     from matplotlib import rcParams
 
     rcParams['ps.fonttype'] = 42
@@ -60,23 +61,25 @@ def plot_models(predictions: dict, y_true, path: str, subject=None):
     tick_size = 12
     label_size = 14
     title_size = 20
-    plt.figure(f"S{subject} GM", figsize=(11, 9))
-    for i, model_name in enumerate(predictions.keys()):
-        plt.subplot(len(predictions.keys()), 1, i+1)
-        plt.plot(time, y_true[:,i], linewidth=2.5, label="measured moment")
-        plt.plot(time, predictions[model_name][:,i],
-                 "r--", linewidth=2, label="prediction")
-        plt.title(model_name, fontsize=title_size)
-        plt.xlim((0, 9))
-        plt.ylabel("Moment [Nm/kg]", fontsize=label_size)
-        plt.yticks([0, 0.5, 1, 1.5], fontsize=tick_size)
-        plt.ylim([-0.25, 1.52])
-        plt.grid(True)
-        plt.axvspan(3, 5.55, alpha=0.3, color='blue')
-    plt.xticks(fontsize=tick_size)
-    plt.xlabel("Time [s]", fontsize=label_size)
-    plt.legend(bbox_to_anchor=(1, -0.5), loc="lower right",
-               borderaxespad=0, ncol=2, fontsize=label_size)
-    plt.tight_layout()
-    plt.draw()
-    plt.savefig(f"{path}S{subject}_models_results.pdf")
+    for j, label in enumerate(labels):
+        plt.figure(f"S{subject} {label}", figsize=(11, 9))
+        for i, model_name in enumerate(predictions.keys()):
+            # Create subplots
+            plt.subplot(len(predictions.keys()), 1, i+1)
+            plt.plot(time, y_true[:, j], linewidth=2.5, label="measured moment")
+            plt.plot(time, predictions[model_name][:, j],
+                    "r--", linewidth=2, label="prediction")
+            plt.title(model_name, fontsize=title_size)
+            plt.xlim((0, 9))
+            plt.ylabel("Moment [Nm/kg]", fontsize=label_size)
+            # plt.yticks([0, 0.5, 1, 1.5], fontsize=tick_size)
+            # plt.ylim([-0.25, 1.52])
+            plt.grid(True)
+            plt.axvspan(3, 5.55, alpha=0.3, color='blue')
+        plt.xticks(fontsize=tick_size)
+        plt.xlabel("Time [s]", fontsize=label_size)
+        plt.legend(bbox_to_anchor=(1, -0.5), loc="lower right",
+                borderaxespad=0, ncol=2, fontsize=label_size)
+        plt.tight_layout()
+        plt.draw()
+        plt.savefig(f"{path}S{subject} {label} estimations.pdf")
