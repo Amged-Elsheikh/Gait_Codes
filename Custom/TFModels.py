30fb42a1d96936cb1eafb45f90fc43a1d3b760a3
Amged <amgedelshiekh@gmail.com>
2022-07-05 14:20:10 +0900

    Add knee model. Formatting

--- Custom/TFModels.py
+++ Custom/TFModels.py
@@ -1,32 +1,15 @@
 from functools import partial
 from tensorflow.keras import layers, models
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
 import tensorflow as tf
 
 
 def create_lstm_model(window_object, stacked=3):
     custom_LSTM = partial(layers.LSTM, units=8, dropout=0.1)
-    
-    # x_input = layers.Input(shape=(window_object.input_width,
-    #                         window_object.features_num))
-    # for i in range(stacked-1):
-    #     if i == 0:
-    #         x = custom_LSTM(return_sequences=True)(x_input)
-    #     else:
-    #         x = custom_LSTM(return_sequences=True)(x)
-    # x = custom_LSTM(return_sequences=False)(x)
-        
-    # x = layers.Dense(window_object.out_nums * window_object.label_width)(x)
-    # x = layers.Reshape([window_object.label_width,
-    #                     window_object.out_nums])(x)
-    # lstm_model = models.Model(inputs=x_input, outputs=x, name="lstm_model")
-    # lstm_model.summary()
-    # return lstm_model
-
     lstm_model = models.Sequential(
         [
             layers.InputLayer((window_object.input_width,
                               window_object.features_num)),
-            # custom_LSTM(return_sequences=True),
             custom_LSTM(return_sequences=True),
             custom_LSTM(return_sequences=False),
             layers.Dense(window_object.out_nums * window_object.label_width),
@@ -43,19 +26,10 @@ def create_conv_model(window_object):
         [
             layers.InputLayer((window_object.input_width,
                               window_object.features_num)),
-            # layers.BatchNormalization(),
             layers.Conv1D(filters=16, kernel_size=3,
                           strides=1, padding="same"),
             layers.BatchNormalization(),
             layers.MaxPool1D(pool_size=3, strides=1, padding="valid"),
-            # layers.Conv1D(filters=64, kernel_size=3,
-            #               strides=1, padding="same"),
-            # layers.BatchNormalization(),
-            # layers.MaxPool1D(pool_size=2, strides=1, padding="valid"),
-            #  layers.Conv1D(filters=128, kernel_size=3,
-            #               strides=1, padding="same"),
-            # layers.Conv1D(filters=window_object.out_nums,
-            #               kernel_size=1, strides=1),
             layers.LSTM(4, return_sequences=True),
             layers.LSTM(4, return_sequences=False),
             layers.Dense(window_object.out_nums * window_object.label_width),
@@ -86,16 +60,34 @@ def create_ff_model(window_object):
     return nn_model
 
 
+def knee_lstm_model(window_object):
+    custom_LSTM = partial(layers.LSTM, units=8, dropout=0)
+    lstm_model = models.Sequential(
+        [
+            layers.InputLayer((window_object.input_width,
+                              window_object.features_num)),
+            custom_LSTM(return_sequences=True),
+            custom_LSTM(return_sequences=False),
+            layers.Dense(window_object.out_nums * window_object.label_width),
+            layers.Reshape([window_object.label_width, window_object.out_nums])]
+    )
+    lstm_model.summary()
+    return lstm_model
+
+
 def model_callbacks(model_file):
-    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-        filepath=model_file, save_weights_only=True,
-        monitor="val_loss", save_best_only=True,)
+    checkpoint_callback = ModelCheckpoint(filepath=model_file,
+                                          save_weights_only=True,
+                                          monitor="val_loss",
+                                          save_best_only=True)
 
-    reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor="val_loss",
-                                                     min_delta=1e-3,
-                                                     factor=0.7,  patience=20)
+    reduce_lr = ReduceLROnPlateau(monitor="val_loss",
+                                  min_delta=1e-3,
+                                  factor=0.7,
+                                  patience=20)
 
-    early_stop = tf.keras.callbacks.EarlyStopping(monitor="val_loss", 
-                                                  patience=50, restore_best_weights=True,)
-    callbacks = [checkpoint_callback, reduce_lr, early_stop]
-    return callbacks
+    early_stop = EarlyStopping(monitor="val_loss",
+                               patience=50,
+                               restore_best_weights=True)
+
+    return [checkpoint_callback, reduce_lr, early_stop]
