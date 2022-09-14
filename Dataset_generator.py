3f4cdabae38e26f432f65ab92a8aa7c6cb557267
Amged-Elsheikh <amgedelshiekh@gmail.com>
2022-09-14 14:58:41 +0900

    Code Documenting

--- Dataset_generator.py
+++ Dataset_generator.py
@@ -1,84 +1,90 @@
-"""This code will create each subject dataset. the dataset will contains on EMG features, Joints kinematics and kinetics.
+"""
+This code will create subject's dataset. the dataset will contains: EMG features, Joints kinematics and kinetics.
 """
 import pandas as pd
 import numpy as np
 from scipy.signal import butter, filtfilt
 import json
+from typing import *
 
 
-def get_directories(subject, trials, use_DEMG=True):
+def get_directories(subject: int, trials, use_DEMG=True) -> List[List[str]]:
+    '''
+    Load all experiment data directories and the dataset directories
+    '''
     # Load the subject details
     with open("subject_details.json", "r") as f:
         subject_details = json.load(f)
         date = subject_details[f"S{subject}"]["date"]
-
+    # Get IK directories
     ik_path = f"../OpenSim/S{subject}/{date}/IK/"
     IK_files = list(map(lambda x: f"{ik_path}S{subject}_{x}_IK.mot", trials))
-
+    # Get ID directories
     id_path = f"../OpenSim/S{subject}/{date}/ID/"
     ID_files = list(
         map(lambda x: f"{id_path}{x}/inverse_dynamics.sto", trials))
-
+    # Get the record's periods directories
     record_periods_path = f"../Outputs/S{subject}/{date}/record_periods/"
     periods_files = list(
         map(lambda x: f"{record_periods_path}S{subject}_{x}_record_periods.csv", trials))
-
+    # Get the EMG features directories
     features_path = f"../Outputs/S{subject}/{date}/EMG/"
     Features_files = list(
         map(lambda x: f"{features_path}{x}_features.csv", trials))
-
+    # Get Dataset directories
     if use_DEMG:
         emg_type = 'DEMG'
     else:
         emg_type = 'sEMG'
-
     output_folder = f"../Dataset/{emg_type}/S{subject}/"
     output_files = list(
         map(lambda x: f"{output_folder}{x}_dataset.csv", trials))
-
-    return IK_files, ID_files, periods_files, Features_files, output_files
-
-
-def lowpass_filter(data, freq=5, fs=100):
-    low_pass = freq/(fs/2)
-    b2, a2 = butter(N=6, Wn=low_pass, btype='lowpass')
-    data.iloc[:, 1:] = filtfilt(b2, a2, data.iloc[:, 1:], axis=0)
-    return data
+    return [IK_files, ID_files, periods_files, Features_files, output_files]
 
 
-def load_data(ik_file, id_file, periods_file, features_file):
+def load_data(ik_file: str, id_file: str, periods_file: str, features_file: str) -> List[pd.DataFrame]:
+    '''
+    This function will load the trial data
+    '''
     # Load IK data
     IK = pd.read_csv(ik_file, header=8, sep='\t', usecols=[0, 17, 18])
     # Load ID data
     ID = pd.read_csv(id_file, header=6, sep='\t', usecols=[0, 17, 19])
     # Load record interval data
     periods = pd.read_csv(periods_file, index_col=0)
-
     # Load EMG features
     features = pd.read_csv(features_file, index_col='time')
-    return IK, ID, periods, features
+    return [IK, ID, periods, features]
 
 
 def merge_joints(IK: pd.DataFrame, ID: pd.DataFrame, periods: pd.DataFrame) -> pd.DataFrame:
-    """Join kinematics and kinetics data into a single pandas dataframe along with record periods dataframe which used to downsample the dataset by removing all periods where ID solution was not available (no GRF data)
+    """Join kinematics and kinetics data into a single pandas dataframe along with record periods
+    dataframe which used to remove the moment values (set as NaN) outside the recording period (space)
     """
+    # Filter the moments
     ID.iloc[:, :] = lowpass_filter(ID)
-    # Merge kinematics and kinetics data
+    # Merge kinematics and kinetics data on the time column
     joints_data = pd.merge(IK, ID, on='time', how='inner')
-
-    left = periods[['left_start', 'left_end']].dropna()
     # work on left side
     moments_columns = ID.columns[1:]
-
+    ######################## Start Removing Moments outside the recording periods ########################
+    # This script assumes left side only. Use dropna to remove the last row (shows how long the recording period was)
+    left = periods[['left_start', 'left_end']].dropna()
+    # First remove all moment values before the staqrt of the recording period
     condition = joints_data['time'] <= left.iloc[0, 0]/100
     joints_data.loc[condition, moments_columns] = np.nan
+    # Loop through the recording periods rows. Remove the moment data between the end of the recording period 
+    # and the start of the following recording period
     for i in range(1, len(left)):
         # divide by 100 to get the time
         previous_end = left.loc[i-1, 'left_end']/100
         current_start = left.loc[i, 'left_start']/100
+        # Set the removing condition as stated above
         condition = (previous_end <= joints_data['time']) & (
             joints_data['time'] <= current_start)
+        # Remove data outside the recording periods
         joints_data.loc[condition, moments_columns] = np.nan
+    # By the endof the looping, the data after the last recording period's end is still available, remove it as well
     condition = joints_data['time'] > left.loc[i, 'left_end']/100
     joints_data.loc[condition, moments_columns] = np.nan
     # Reset time to zero to match EMG
@@ -86,51 +92,67 @@ def merge_joints(IK: pd.DataFrame, ID: pd.DataFrame, periods: pd.DataFrame) -> p
     return joints_data
 
 
+def lowpass_filter(data: pd.DataFrame, freq=5, fs=100):
+    low_pass = freq/(fs/2)
+    b2, a2 = butter(N=6, Wn=low_pass, btype='lowpass')
+    data.iloc[:, 1:] = filtfilt(b2, a2, data.iloc[:, 1:], axis=0)
+    return data
+
+
 def reset_time(data: pd.DataFrame) -> pd.DataFrame:
-    start_time = data['time'].min()
-    data['time'] = data['time'].apply(lambda x: x-start_time)
+    '''
+    This function will reset the time from zero by removing the minimum value
+    '''
+    # start_time = data['time'].min()
+    # data['time'] = data['time'].apply(lambda x: x-start_time)
+    data['time'] = data['time'] - data['time'].min()
+    # Round the time into 3 digits (very important because of python limitations)
     data['time'] = np.around(data['time'], 3)
     return data
 
 
 def merge_IO(features: pd.DataFrame, joints_data: pd.DataFrame) -> pd.DataFrame:
     """
-    downsampling is hold while merging by removing points
+    This function will create the dataset by merging the inputs and outputs.
+    IK and ID data downsampling is hold while merging by removing time point
+    that's are not shared between the features and joints data
     """
-    # Merge all features and joints. Down sampling is done while merging
+    # Merge all features and joints. Down sampling is done while merging using 'inner' argument
     Dataset = pd.merge(left=features, right=joints_data,
                        on='time', how='inner')
     Dataset.set_index("time", inplace=True)
     return Dataset
 
 
-def get_dataset(subject=None, use_DEMG=False) -> None:
-    # If subject number wasn't provided ask the user to manually input it
-    if subject == None:
-        subject = input("Please write subject number in a format XX: ")
+def get_dataset(subject: Union[str, int], use_DEMG: bool = False):
+    try:
+        subject = f"{int(subject):02d}"
+    except:
+        raise 'Subject variable should be a number'
     # Experiment trials names
     trials = ('train_01', 'train_02', 'val', 'test')
     ########################### Get I/O directories ###########################
     IK_files, ID_files, periods_files, Features_files, output_files = get_directories(
-                                                                        subject, trials, use_DEMG)
+        subject, trials, use_DEMG)
     ########################### Loop in each trial ###########################
-    for ik_file, id_file, periods_file, features_file, output_name\
-            in zip(IK_files, ID_files, periods_files, Features_files, output_files):
-
-        IK, ID, periods, features = load_data(
-            ik_file, id_file, periods_file, features_file)
+    for trial in range(len(trials)):
+        # Load the data
+        IK, ID, periods, features = load_data(IK_files[trial], ID_files[trial],
+                                              periods_files[trial], Features_files[trial])
         # Merge IK, ID & record intervals together to create joint's dataset
         joints_data = merge_joints(IK, ID, periods)
         # Merge EMG features with joints data and down sample joints data to match the EMG features
         Dataset = merge_IO(features, joints_data)
         # Rename column and save the dataset
-        new_col = {'knee_angle_l': "knee angle",  # Test as input
-                   'ankle_angle_l': 'ankle angle',  # will not be used
+        new_col = {'knee_angle_l': "knee angle",
+                   'ankle_angle_l': 'ankle angle',
                    'ankle_angle_l_moment': "ankle moment",
                    'knee_angle_l_moment': "knee moment"}
         Dataset.rename(columns=new_col, inplace=True)
-        Dataset.to_csv(output_name)
+        # Save the dataset
+        Dataset.to_csv(output_files[trial])
 
 
 if __name__ == "__main__":
-    get_dataset()
+    subject = input('Please write the subject number: ')
+    get_dataset(subject=subject, use_DEMG=False)
--- Force_convert_functions.py
+++ Force_convert_functions.py
@@ -1,182 +1,31 @@
+'''
+This script will process the data from force plate 1 only. 
+'''
 import json
 import os
 import re
-from scipy.signal import butter, filtfilt
+from typing import *
+
 import numpy as np
 import pandas as pd
+from scipy.signal import butter, filtfilt
 
 pd.set_option('mode.chained_assignment', None)
 
 
-def get_IO_dir(subject, trials):
-    with open("subject_details.json", "r") as f:
-        subject_details = json.load(f)
-
-    date = subject_details[f"S{subject}"]["date"]
-    input_path = f"../Data/S{subject}/{date}/Dynamics/Force_Data/"
-    output_path = f"../OpenSim/S{subject}/{date}/Dynamics/Force_Data/"
-    files = [f"S{subject}_{trial}_forceplate_1.csv" for trial in trials]
-    return input_path, output_path, files
-
-
-def remove_system_gap(data):
-    """
-    In some cases force plates stop recording and send only zeros. This function will\\
-        first set these values for NaN and then interpolate missing values.
-    """
-    columns = [' Fx', ' Fy', ' Fz',
-               ' Mx', ' My', " Mz"]
-
-    data.loc[data.loc[:, ' Fz'] == 0, columns] = np.nan
-    data.iloc[:, :] = data.interpolate(method="linear")
-    data.iloc[:, :] = data.fillna(method="bfill")
-    return data
-
-
-def trial_period(data, subject, trial):
-    with open("subject_details.json", "r") as f:
-        subject_details = json.load(f)
-
-    record_period = subject_details[f"S{subject}"]["motive_sync"][trial]
-    fps = subject_details[f"S{subject}"]['FP_sampling_rate']
-    data['time'] = data[" DeviceFrame"]/fps
-    record_start = int(record_period['start']*fps)
-    record_end = int(record_period['end']*fps)
-    data = data.iloc[record_start:record_end+1, :]
-    data.reset_index(inplace=True, drop=True)
-    return data
-
-
-def remove_offset(data, remove=True):
-    if remove:
-        # Choose Forces and Moments
-        columns = [" Fx", " Fy", " Fz", " Mx", " My", " Mz"]
-        for col in columns:
-            data.loc[:, col] = data.loc[:, col] - data.loc[5:15, col].mean()
-    return data
-
-
-def grf_periods(data, trigger=5):
-    stance_periods = []
-    start = None
-    for i, point in enumerate(data[" Fz"]):
-        # set starting point if this is the first point
-        if point >= trigger and start == None:
-            start = data.loc[i, "MocapFrame"]
-        elif point < trigger and start != None:
-            # sometimes the subject might enter forceplate and leave it immediately because he enter with wrong foot. the filter will raise error if the number of points is less than 15
-            if data.loc[i, "MocapFrame"]-start >= 20:
-                stance_periods.append(slice(start, data.loc[i, "MocapFrame"]))
-            start = None
-    return stance_periods
-
-
-def apply_filter(data, subject, trigger=5):
-    with open("subject_details.json", "r") as f:
-        subject_details = json.load(f)
-
-    stance_periods = grf_periods(data, trigger)
-    fs = subject_details[f"S{subject}"]["FP_sampling_rate"]  # Hz
-    f = fs/20  # Filter frequency
-    low_pass = f/(fs/2)
-    b2, a2 = butter(N=6, Wn=low_pass, btype='lowpass')
-    columns = [" Fx", " Fy", " Fz",
-               " Mx", " My", " Mz", ]
-
-    data[' Cx'] = -data[' My']/data[' Fz']
-    data[' Cy'] = data[' Mx']/data[' Fz']
-    data[' Cx'].loc[abs(data[' Cx']) > 0.25] = 0
-    data[' Cy'].loc[abs(data[' Cy']) > 0.25] = 0
-    # apply filter
-    for stance in stance_periods:
-        condition = (data["MocapFrame"] >= stance.start-5) & (
-            data["MocapFrame"] <= stance.stop+5)
-        data.loc[condition, columns] = filtfilt(
-            b2, a2, data.loc[condition, columns], axis=0)
-    # CoP are calculated from the Force and Moment. Filter CoP by recalculate it from the filtered data. Note that the CoP will grow when foot outside force plate.
-        data.loc[condition, " Cx"] = - \
-            data.loc[condition, " My"]/data.loc[condition, " Fz"]
-        data.loc[condition, " Cy"] = data.loc[condition,
-                                              " Mx"] / data.loc[condition, " Fz"]
-    return data
-
-
-def system_match(data):
-    """
-    Match opti-track and force plates axes.
-    Remove system gaps if any.
-    """
-    # To apply rotation, change column names.
-    col_names = {" Fx": " Fx", " Fy": " Fz", " Fz": " Fy",
-                 " Mx": " Mx", " My": " Mz", " Mz": " My",
-                 " Cx": " Cx", " Cy": " Cz", " Cz": " Cy", }
-    data.rename(columns=col_names, inplace=True)
-    # Match opti-track and force Plates origins
-    data.loc[:, " Cx"] = data[" Cx"] + 0.25
-    data.loc[:, " Cz"] = data[" Cz"] + 0.25
-    # Complete the rotation
-    change_sign = [" Fx", " Fz"]
-    data.loc[:, change_sign] = -data.loc[:, change_sign]
-    return data
-
-
-# There is a delay in system (various delay may change )
-def shift_data(data, subject,shift_key):
-    with open("subject_details.json", "r") as f:
-        subject_details = json.load(f)
-
-    shift_value = subject_details[f"S{subject}"]["delay"][shift_key][0]
-    if shift_value != 0:
-        shift_columns = [' Fx', ' Fz', ' Fy',
-                         ' Mx', ' Mz', ' My',
-                         ' Cx', ' Cz', ' Cy']
-        data.loc[:, shift_columns] = data[shift_columns].shift(
-            shift_value, fill_value=0)
-    return data
-
-
-def col_rearrange(data):
-    return data[["time", " Fx", " Fy", " Fz", " Mx", " My", " Mz", " Cx", " Cy", " Cz"]]
-
-
-def GRF_data(data):
-    # make sure columns are weell arranged
-    data = col_rearrange(data)
-    L_columns_names_mapper = {" Fx": "1_ground_force_vx",
-                              " Fy": "1_ground_force_vy",
-                              " Fz": "1_ground_force_vz",
-                              " Cx": "1_ground_force_px",
-                              " Cy": "1_ground_force_py",
-                              " Cz": "1_ground_force_pz",
-                              " Mx": "1_ground_torque_x",
-                              " My": "1_ground_torque_y",
-                              " Mz": "1_ground_torque_z"}
-    data.rename(columns=L_columns_names_mapper, inplace=True)
-    return data
-
-
-def save_force_data(force_data, output_path, output_name):
-    output_file = output_path + output_name
-    if os.path.exists(output_file):
-        os.remove(output_file)
-    force_data.to_csv(output_file,  sep='\t', index=False)
-    nRows = len(force_data)  # end_time - start_time + 1
-    nColumns = len(force_data.columns)
-
-    with open(output_file, "r+") as f:
-        old = f.read()  # read everything in the file
-        f.seek(0)  # rewind
-        f.write(output_name+'\n' + 'version=1\n' +
-                f'nRows={nRows}\n' + f'nColumns={nColumns}\n' + 'inDegrees=yes\n' + 'endheader\n' + old)
-
-
-def GRF_to_OpenSim(subject, trials=["train_01", "train_02", "val", "test"], use_filter=False, offset_remove=False):
-    if subject == None:
-        subject = input(f"insert subject number: ")
-    subject = f"{int(subject):02d}"
-
+def GRF_to_OpenSim(subject: Union[str, int], trials: List[str] = ['test', ], 
+                   use_filter: bool = False, offset_remove: bool = False) -> None:
+    '''
+    This function will handle finding the inputs and outputs directories, 
+    process the input data from csv format to sto format which will be used
+    by OpenSim software later to solve the inverse dynamic equation. 
+    '''
+    try:
+        subject = f"{int(subject):02d}"
+    except:
+        raise 'Subject variable should be a number'
+    # Get the inputs and outputs directories
     input_path, output_path, files = get_IO_dir(subject, trials)
-
     # Process each trial
     for i, file in enumerate(files):
         # Load Left force plates data
@@ -192,8 +41,7 @@ def GRF_to_OpenSim(subject, trials=["train_01", "train_02", "val", "test"], use_
         data = shift_data(data, subject, shift_key=trials[i])
 
         # Remove the offset from the data
-        if offset_remove:
-            data = remove_offset(data)
+        data = remove_offset(data, remove=offset_remove)
 
         # Apply low pass filter
         if use_filter:
@@ -209,11 +57,235 @@ def GRF_to_OpenSim(subject, trials=["train_01", "train_02", "val", "test"], use_
         save_force_data(force_data, output_path, output_name)
 
 
-if __name__ == '__main__':
-    trials = ["train_01", "train_02", "val", "test"]
-    subject = None
-    if subject:
-        subject = f"{int(subject):02d}"
+def get_IO_dir(subject: str, trials: List[str]) -> Tuple[str, str, List[str]]:
+    """
+    This fynction will generate inputs and outputs directories for the selected\
+    subject and trials.
+    """
+    # Load experiment information
+    with open("subject_details.json", "r") as f:
+        subject_details = json.load(f)
 
-    GRF_to_OpenSim(subject, use_filter=False, offset_remove=False,
-                   trials=["train_01", "train_02", "val", "test"])
+    date = subject_details[f"S{subject}"]["date"]
+    input_path = f"../Data/S{subject}/{date}/Dynamics/Force_Data/"
+    output_path = f"../OpenSim/S{subject}/{date}/Dynamics/Force_Data/"
+    files = [f"S{subject}_{trial}_forceplate_1.csv" for trial in trials]
+    return input_path, output_path, files
+
+
+def trial_period(data: pd.DataFrame, subject: str, trial: str) -> pd.DataFrame:
+    '''
+    this function will trim the experiment to the actual experiment period and\
+    create the time column
+    '''
+    # Load experiment information
+    with open("subject_details.json", "r") as f:
+        subject_details = json.load(f)
+    # get the actual experiment period and force plates sampling rate
+    record_period = subject_details[f"S{subject}"]["motive_sync"][trial]
+    fps = subject_details[f"S{subject}"]['FP_sampling_rate']
+    # Create time column by dividing frame number by the sampling rate
+    data['time'] = data[" DeviceFrame"]/fps
+    # Trim the experiment to the actual experiment period
+    record_start = int(record_period['start']*fps)
+    record_end = int(record_period['end']*fps)
+    data = data.iloc[record_start:record_end+1, :]
+    # Reset the index column but keep the time column
+    data.reset_index(inplace=True, drop=True)
+    return data
+
+
+def remove_system_gap(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    In some cases force plates stop recording and send zeros.it's not common to have
+    exactly 0, usually the reading will be small float number
+    This function will first set these values to NaN and then perform linear interpolation.
+    """
+    # Get the main sensors' data from the dataset. CoP is calculated based on the force and moment
+    # not measured by a sensor directly.
+    columns = [' Fx', ' Fy', ' Fz',
+               ' Mx', ' My', " Mz"]
+    # Zero means no data sent in almost all cases
+    data.loc[data.loc[:, ' Fz'] == 0, columns] = np.nan
+    data.iloc[:, :] = data.interpolate(method="linear")
+    data.iloc[:, :] = data.fillna(method="bfill")
+    return data
+
+
+def shift_data(data: pd.DataFrame, subject, shift_key) -> pd.DataFrame:
+    """
+    In early experements there was no external synchronization between the Mocap
+    and force plates, resulting in a starting delay. This delay is different every
+    time the start button is pressed to start new experiment/trial. 
+
+    This function will shift the data by a number of frames specified by the user
+    in the experiment json file with 'delay' as a key value.
+    """
+    with open("subject_details.json", "r") as f:
+        subject_details = json.load(f)
+
+    shift_value = subject_details[f"S{subject}"]["delay"][shift_key][0]
+    if shift_value != 0:
+        shift_columns = [' Fx', ' Fz', ' Fy',
+                         ' Mx', ' Mz', ' My',
+                         ' Cx', ' Cz', ' Cy']
+        data.loc[:, shift_columns] = data[shift_columns].shift(
+            shift_value, fill_value=0)
+    return data
+
+
+def remove_offset(data: pd.DataFrame, remove: bool = True) -> pd.DataFrame:
+    '''
+    Force plate sensors have a small amount of offset. It can be removed bt
+    finding the average offset value and substracting from the dataset if the 
+    user want to do so.
+    '''
+    if remove:
+        # Choose Forces and Moments
+        columns = [" Fx", " Fy", " Fz", " Mx", " My", " Mz"]
+        for col in columns:
+            data.loc[:, col] = data.loc[:, col] - data.loc[5:15, col].mean()
+    return data
+
+
+def apply_filter(data: pd.DataFrame, subject: str) -> pd.DataFrame:
+    '''
+    This function is responsible for applying a Butterworth lowpass filter
+    for the force and moment data when the subject is on the force plate.
+    The filter low frequency will be adjusted automatically depending on
+    the sample rate of the force plate.
+
+    The function will detect when the subject is on the force plate if
+    the Fz value is greater the 10% of the subject's weight.
+
+    '''
+    # Load experiment information
+    with open("subject_details.json", "r") as f:
+        subject_details = json.load(f)
+    # Set the trigger value and get the fps
+    trigger = 0.1 * float(subject_details[f"S{subject}"]['weight'])
+    fs = subject_details[f"S{subject}"]["FP_sampling_rate"]  # Hz
+    # Get the periods when subject is on the force plate.
+    stance_periods = grf_periods(data, trigger)
+    # Create the lowpass filter based on the forceplate sampling rate
+    f = fs/20  # Filter frequency
+    low_pass = f/(fs/2)
+    b2, a2 = butter(N=6, Wn=low_pass, btype='lowpass')
+    # select the columns to be filtered
+    columns = [" Fx", " Fy", " Fz",
+               " Mx", " My", " Mz", ]
+    # Recalculate the CoP
+    data[' Cx'] = -data[' My']/data[' Fz']
+    data[' Cy'] = data[' Mx']/data[' Fz']
+    # If the CoP is outside the force plate, put it at it's center
+    data[' Cx'].loc[abs(data[' Cx']) > 0.25] = 0
+    data[' Cy'].loc[abs(data[' Cy']) > 0.25] = 0
+    # apply the filter
+    for stance in stance_periods:
+        # Set the conditions for the data to be filtered
+        condition = (data["MocapFrame"] >= stance.start-5) &\
+            (data["MocapFrame"] <= stance.stop+5)
+        # Apply the filter
+        data.loc[condition, columns] = filtfilt(
+            b2, a2, data.loc[condition, columns], axis=0)
+    # CoP are calculated from the Force and Moment.
+    # Filter CoP by recalculate it from the filtered data.
+    # Note that the CoP will grow when foot outside force plate
+    # since Fz is close to zero.
+        data.loc[condition, " Cx"] = - \
+            data.loc[condition, " My"]/data.loc[condition, " Fz"]
+
+        data.loc[condition, " Cy"] = \
+            data.loc[condition, " Mx"] / data.loc[condition, " Fz"]
+    return data
+
+
+def grf_periods(data: pd.DataFrame, trigger: float = 5.0) -> List[range]:
+    '''
+    This function will return a list of periods when the subject 
+    was on the force plate.
+    '''
+    stance_periods = []
+    start = None
+    for i, point in enumerate(data[" Fz"]):
+        # set starting point if this is the first point
+        if point >= trigger and start == None:
+            start = data.loc[i, "MocapFrame"]
+        elif point < trigger and start != None:
+            # sometimes the subject might enter forceplate and leave
+            # it immediately because (s)he enters with wrong foot. the
+            # filter will raise error if the number of points is less
+            # than 15 frame.
+            if data.loc[i, "MocapFrame"]-start >= 20:
+                stance_periods.append(slice(start, data.loc[i, "MocapFrame"]))
+            start = None
+    return stance_periods
+
+
+def system_match(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    This funchion will match opti-track and force plates axes,
+    so that OpenSim can correctly solve the inverse dynamic equation.
+    """
+    # To apply rotation, change column names.
+    col_names = {" Fx": " Fx", " Fy": " Fz", " Fz": " Fy",
+                 " Mx": " Mx", " My": " Mz", " Mz": " My",
+                 " Cx": " Cx", " Cy": " Cz", " Cz": " Cy", }
+    data.rename(columns=col_names, inplace=True)
+    # Complete the rotation
+    change_sign = [" Fx", " Fz"]
+    data.loc[:, change_sign] = -data.loc[:, change_sign]
+    # Match opti-track and force Plates origins
+    data.loc[:, " Cx"] = data[" Cx"] + 0.25
+    data.loc[:, " Cz"] = data[" Cz"] + 0.25
+    return data
+
+
+def GRF_data(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Format the columns name to match the names in OpenSim tutorials.
+    It's possible to skip this phase, but I won't recommend it at all.
+    """
+    # make sure columns are well arranged to save time when working with OpenSim.
+    data = data[["time", " Fx", " Fy", " Fz",
+                 " Mx", " My", " Mz",
+                 " Cx", " Cy", " Cz"]]
+    # Rename the columns
+    columns_names_mapper = {" Fx": "1_ground_force_vx",
+                              " Fy": "1_ground_force_vy",
+                              " Fz": "1_ground_force_vz",
+                              " Cx": "1_ground_force_px",
+                              " Cy": "1_ground_force_py",
+                              " Cz": "1_ground_force_pz",
+                              " Mx": "1_ground_torque_x",
+                              " My": "1_ground_torque_y",
+                              " Mz": "1_ground_torque_z"}
+    data.rename(columns=columns_names_mapper, inplace=True)
+    return data
+
+
+def save_force_data(force_data: pd.DataFrame, output_path: str, output_name: str) -> None:
+    '''
+    This function is used to convert processed force plate data (Pandas/CSV) to OpenSim 
+    format.
+    '''
+    output_file = output_path + output_name
+    if os.path.exists(output_file):
+        os.remove(output_file)
+    force_data.to_csv(output_file,  sep='\t', index=False)
+    nRows = len(force_data)  # end_time - start_time + 1
+    nColumns = len(force_data.columns)
+
+    with open(output_file, "r+") as f:
+        old = f.read()  # read everything in the file
+        f.seek(0)  # rewind
+        f.write(output_name+'\n' + 'version=1\n' +
+                f'nRows={nRows}\n' + f'nColumns={nColumns}\n' + 'inDegrees=yes\n' + 'endheader\n' + old)
+
+
+if __name__ == '__main__':
+    subject = input('Please write the subject number: ')
+    trials = ["train_01", "train_02", "val", "test"]
+
+    GRF_to_OpenSim(subject=subject, trials=trials,
+                   use_filter=False, offset_remove=False,)
